342ad11fe78746c97bd430bfa968173ececfb38c
Yeronn <yeronn.f@gmail.com>
2024-11-19 11:22:42 -0500

    refactor(dto): organized DTOs into subfolders

--- Application/DTOs/UpdateResponse.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Application.DTOs
-{
-    public record UpdateResponse (string Message = null!, string Name = null!);
-}
--- Application/DTOs/UsersDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.DTOs.User
-{
-    public class UsersDto
-    {
-        public int Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Fullname { get; set; } = string.Empty;
-    }
-}
